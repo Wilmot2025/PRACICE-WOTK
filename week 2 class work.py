3fde45d2c19001d3f8ec307e763925d5695ee277
Wilmot2025 <wilmotokai@gmail.com>
2025-10-07 17:13:20 +0000

    using variable

---
+++ week 2 class work.py
@@ -0,0 +1,19 @@
+x = "John"  
+# x is a variable
+print(x)
+
+carname = "Toyota"
+print(carname)
+car_model = "Corolla"
+print(car_model)
+car_year = 2020
+print(car_year) 
+car_color = "Blue"
+print(car_color)   
+car_price = 20000
+print(car_price)
+car_mileage = 15000
+print(car_mileage)
+car_owner = "Wilmot Okai"
+
+
