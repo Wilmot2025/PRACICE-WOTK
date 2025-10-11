ca40be372c439f5d9b4e5b7e40a78b1932330b1a
Wilmot2025 <wilmotokai@gmail.com>
2025-10-11 13:47:34 +0000

    concatenation, repetition, interger, float, addtion, subtraction, multipication, divison modulus, floor division, operators

---
+++ Testing.py
@@ -0,0 +1,33 @@
+First_name= "wilmot"
+Last_name = "Okai"
+Full_name = First_name + " " + Last_name
+print(Full_name) # Concatenation
+
+my_name = "Wilmot"
+print(my_name * 5) # Repetition
+
+age = 30
+print(age) # Integer
+
+age = 30.5
+print(age) # Float
+int= 10
+float= 20.5
+print(int + float) # Addition   
+a=10
+b=5
+print(a - b) # Subtraction
+a=10*5  
+print(a) # Multiplicat
+a=10/5
+print(a) # Division
+print(a) # Modulus
+print(a) # Floor Division
+
+my_string="Wilmot"
+Print(my_string.upper()) 
+my_string=("Wilmot" )
+print(my_string.lower()) 
+my_string =my_string.split (",")
+new_string = my_string.replace("Wilmot", "Okai")  
+
