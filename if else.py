63caa957defc2708e2263f30afefa451c5589930
Wilmot2025 <wilmotokai@gmail.com>
2025-10-25 14:29:41 +0000

    adding input function

--- if else.py
+++ if else.py
@@ -24,3 +24,31 @@ if a == 10:
 
 if b == 20:
  print("b is equal to 20 , okai! Nisha")
+
+if c == 30:
+ print("c is equal to 30")
+if a < b:
+    print("a is less than b")
+if a > b:
+    print("a is greater than b")
+
+    if age >= 18:
+          if citizen == 'indian':
+                    print("You are eligible to vote")
+          else:
+                    print("YOu must be an indian citizen to vote")
+
+else: 
+                    print("you are not eligible to vote")
+
+
+age = int(input("enter your age"))
+citizen = input("enter your citizenship")
+if age >= 18:
+          if citizen.lower() == 'indian':
+                    print("You are eligible to vote")
+          else:
+                    print("YOu must be an indian citizen to vote")
+
+else: 
+                    print("you are not eligible to vote")
