ae06381a07417ce28925c71c890a1d9bcdcb345f
Wilmot2025 <wilmotokai@gmail.com>
2025-10-25 14:23:00 +0000

    using equal to

--- if else.py
+++ if else.py
@@ -1,7 +1,8 @@
-if a=b
-a=5
-b=10
-if a > b:
+a = 5
+b = 10
+if a == b:
+    print("a is equal to b")
+elif a > b:
     print("a is greater than b")    
 elif a < b:
     print("a is less than b")
@@ -13,3 +14,13 @@ b=10
 
 if a > b:
     print("a is greater than b")
+
+a = 10
+b = 20
+c = 30
+
+if a == 10:
+ print("a is equal to 10")
+
+if b == 20:
+ print("b is equal to 20 , okai! Nisha")
