01ee13568ff09ce010260caf4b614c9d70341704
Wilmot2025 <wilmotokai@gmail.com>
2025-10-25 15:17:19 +0000

    pratice work

--- look.py
+++ look.py
@@ -1,14 +1,24 @@
-print("Hello, World!")  
-first_name =("John")
-last_name =("Doe")  
-full_name = (first_name + " " + last_name) 
-print("John Doe")
 
-age = 30
-print(" age")    
-print("30")
+for i in range(1,11):
+    print(i)
+    print("Wilmot")
+    print("Okai")
+    print("wilmot okai jr")
 
+# Second loop to print Sharik 5 times
+for i in range(5):
+    print("Sharik")
 
-a = 'Hello'
-b = 'World'
-print(a + b)
+# Loop steps explanation:
+# step 1: i starts at 0 (initialization)
+# step 2: check if i < 5 (condition)
+# step 3: print "Sharik" (task)
+# step 4: i increases by 1 (increment)
+
+for i in range(5):
+    print("Sharik")
+print("Wilmot Okai Jr")
+for i in range(2, 21, 2):
+    print(i)    
+for i in range(1, 21, 2):
+    print(i)
