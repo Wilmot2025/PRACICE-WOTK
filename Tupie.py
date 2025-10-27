ee8339024863afc24ce354803ac03e6643658386
Wilmot2025 <wilmotokai@gmail.com>
2025-10-27 17:17:21 +0000

    using order, immutable collection

---
+++ Tupie.py
@@ -0,0 +1,21 @@
+# woring witn tupie
+numone = ('abcdefghij')
+print(numone[1:5])  
+
+lenth = len(numone)     
+print(lenth)
+
+numtwo = (1,2,3,4,5,6,7,8,9,10)
+print(numtwo[2:5])  
+
+numthree = (1,2,3,4,5,6,7,8,9,10)   
+print(numthree [1:5:2]) 
+
+
+num=(3,)    
+print(num)
+
+num=(3,4,5,6,7,8,9,10)    
+print(num [7]) 
+length = len(num)
+print(length)
