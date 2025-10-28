7825e633b8752abbcb6b08013e655c71ed56e8c9
Wilmot2025 <wilmotokai@gmail.com>
2025-10-28 16:48:42 +0000

    using string

--- even and odd Nmber.py
+++ even and odd Nmber.py
@@ -1,10 +1,17 @@
-sum_even =0
+sum_even = 0
 prodct_odd = 1
-for i in range (10,31):
- if i % 2==0:
-  sum_even += i
-else:
-       prodct_odd *= i
+for i in range(10, 31):
+    if i % 2 == 0:
+        sum_even += i
+    else:
+        prodct_odd *= i
 
 print("Sum of even numbers from 10 to 30:", sum_even)
-print("Product of odd numbers from 10 to 30:", prodct_odd)  
+print("Product of odd numbers from 10 to 30:", prodct_odd)
+
+string = "concatination"
+print("Original string:", string)
+
+for ch in string:
+    if ch in 'aei':
+        print(ch)
