3ec8653c7198c2b19a32b3ba3886b64ebfaa9909
Wilmot2025 <wilmotokai@gmail.com>
2025-10-28 15:38:02 +0000

    using evening number

---
+++ even and odd Nmber.py
@@ -0,0 +1,10 @@
+sum_even =0
+prodct_odd = 1
+for i in range (10,31):
+ if i % 2==0:
+  sum_even += i
+else:
+       prodct_odd *= i
+
+print("Sum of even numbers from 10 to 30:", sum_even)
+print("Product of odd numbers from 10 to 30:", prodct_odd)  
