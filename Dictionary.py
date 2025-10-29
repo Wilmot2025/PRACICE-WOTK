55b2f555ba1d325219d110de949de268af2cb222
Wilmot2025 <wilmotokai@gmail.com>
2025-10-29 20:34:26 +0000

    dictionary comprehension

--- Dictionary.py
+++ Dictionary.py
@@ -8,3 +8,16 @@ dict_data = {
 
 
 print(dict_data)
+
+
+#Dictionary Methods
+print(dict_data.keys())
+print(dict_data.values())   
+print(dict_data.items())
+print(dict_data.get("name"))
+print(dict_data.get("gender"))
+
+dict_data["age"] = 31
+dict_data["gender"] = " Male
+
+#dictionary Comprehension
--- Javapraticework.java/ifelseexample.java
+++ Javapraticework.java/ifelseexample.java
@@ -1,42 +1,41 @@
-public class IfElseExample{
-}
-    public static void main (String []args){
-      int number =10;
-      if(number > 15) {
-        System.out.println("The number is greater than 15.");}
-    else{
-      System.out.println("The number is 15 or less.");
-
-      }
-
-    }     
-  
- public class IfElseExample2{
-   
- }
-  
-    public static void main(String []args){
-      int number =20;
-      if(number % 2==0){
-        System.out.println("The number is even.");
-      }
-      else{
-        System.out.println("The number is odd.");
-      }
-  
+public class IfElseExample {
+    public static void main(String[] args) {
+        example1();
+        example2();
+        example3();
     }
 
-    public class IfElseExample2 {
-    public static void main(String[] args) {
+    private static void example1() {
         int number = 10;
+        if (number > 15) {
+            System.out.println("The number is greater than 15.");
+        } else {
+            System.out.println("The number is 15 or less.");
+        }
+    }
 
-if (number > 0) {
-  System.out.println("The number is positive.");
-   } else if (number < 0) {
-   System.out.println("The number is negative.");
-       
-} else {
+    private static void example2() {
+        int number = 20;
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+
+    private static void example3() {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
             System.out.println("The number is zero.");
         }
     }
+}
+
+int number = 20;
+if (number > 5) {
+  System.out.println("Greater than 5");
 }
