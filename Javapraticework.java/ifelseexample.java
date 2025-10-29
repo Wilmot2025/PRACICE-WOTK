bfc9e7388495b089173e94ab126d23c89d4446e5
Wilmot2025 <wilmotokai@gmail.com>
2025-10-29 10:58:25 +0000

    using if statement

---
+++ Javapraticework.java/ifelseexample.java
@@ -0,0 +1,42 @@
+public class IfElseExample{
+}
+    public static void main (String []args){
+      int number =10;
+      if(number > 15) {
+        System.out.println("The number is greater than 15.");}
+    else{
+      System.out.println("The number is 15 or less.");
+
+      }
+
+    }     
+  
+ public class IfElseExample2{
+   
+ }
+  
+    public static void main(String []args){
+      int number =20;
+      if(number % 2==0){
+        System.out.println("The number is even.");
+      }
+      else{
+        System.out.println("The number is odd.");
+      }
+  
+    }
+
+    public class IfElseExample2 {
+    public static void main(String[] args) {
+        int number = 10;
+
+if (number > 0) {
+  System.out.println("The number is positive.");
+   } else if (number < 0) {
+   System.out.println("The number is negative.");
+       
+} else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
