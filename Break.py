804a4e455e1455cf0a676c8586a9c8f4a412b8f0
Wilmot2025 <wilmotokai@gmail.com>
2025-10-31 13:33:31 +0000

    using the break function

---
+++ Break.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    if i == 3:
+        break   
+    print(i)    #break is used to stop the loop when the condition is met   
+# Output:
+
+for i in range(0,10):
+    if i == 8:
+        break   
+    print(i)    
+    
+    
+    #continue is used to skip the current iteration when the condition is met
