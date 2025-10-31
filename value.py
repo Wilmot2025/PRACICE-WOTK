c555d7202fee04dee6971ac69fabde8530387ad6
Wilmot2025 <wilmotokai@gmail.com>
2025-10-31 14:13:26 +0000

    changing key to value value to key

---
+++ value.py
@@ -0,0 +1,10 @@
+## changing from Key to value
+d= {'a':1, 'b':2, 'c':3}
+print(d['b'])  ## accessing value using key 
+
+print(d)    
+d['c']= 2 
+
+## changing value of key    
+# before changing value
+  ## changing value of key 'b' to 5      
