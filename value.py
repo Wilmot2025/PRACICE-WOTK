35c79864dbbd4dd4acc089dd9b48d8badea8ba4d
Wilmot2025 <wilmotokai@gmail.com>
2025-11-02 14:30:26 +0000

    changing value to key

--- value.py
+++ value.py
@@ -4,7 +4,15 @@ print(d['b'])  ## accessing value using key
 
 print(d)    
 d['c']= 2 
+print(d)
 
+d[('a','b','c')]= 5
+print("a", "b", "c", "after adding 5 to value", + 5)
 ## changing value of key    
 # before changing value
-  ## changing value of key 'b' to 5      
+## changing value of key 'b' to 5      
+d['b']= 5
+print(d)
+## after changing value
+## changing value of key 'b' to 5
+    
