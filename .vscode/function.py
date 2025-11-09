21493281126ff13acc3c010e181c4231ff2ecffc
Wilmot2025 <wilmotokai@gmail.com>
2025-11-09 16:52:46 +0000

    working with function

---
+++ .vscode/function.py
@@ -0,0 +1,18 @@
+num=[1,2,3,4,5]
+# (((1+2)+3)+4)+5
+def sum(num):
+    total=0
+    for i in num:
+        total+=i
+    return total    
+
+print(sum(num)) 
+
+
+l1=[12,13,1,2,3,45]
+
+def sum(l1):    
+    total=0
+    for i in l1:
+        total+=i
+    return total    
