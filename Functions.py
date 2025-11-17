b0b202e42b035e01ac99dad1b5dc99bf7e6cff41
Wilmot2025 <wilmotokai@gmail.com>
2025-11-17 14:28:51 +0000

    Calling the function

--- Functions.py
+++ Functions.py
@@ -8,5 +8,10 @@ def my_function():
 
 print("This is the first time i am calling my function")
 my_function()  # function call
+
 print("This is the second time i am calling my function")
+my_function()   
+
+
+
 
