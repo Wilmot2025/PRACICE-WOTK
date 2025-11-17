4aa3bd5b1d8c7c68fcf7d35486be35fd41625cba
Wilmot2025 <wilmotokai@gmail.com>
2025-11-17 14:27:56 +0000

    Defining Function

---
+++ Functions.py
@@ -0,0 +1,12 @@
+# Function => 
+# what is function in python =>
+
+def my_function():
+    print("This is Suhani")
+    print("this is Nisha")
+    print("this is Sharik")
+
+print("This is the first time i am calling my function")
+my_function()  # function call
+print("This is the second time i am calling my function")
+
