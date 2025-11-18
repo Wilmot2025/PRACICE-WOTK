578f8ab7140635c97209d8d10b79a06fe8770d50
Wilmot2025 <wilmotokai@gmail.com>
2025-11-18 14:42:39 +0000

    operator in indentity operator

--- Identity.py
+++ Identity.py
@@ -4,3 +4,13 @@ suhani_list = [ 1, 2, 3, 4, 5 ]
 suhani_list1 = [ 1, 2, 3, 4, 5 ]    
 print ( suhani_list is suhani_list1 ) # False
 print ( suhani_list == suhani_list1 ) # True
+
+# is operator is use for identity operator.
+# == operator is use for equality operator.
+
+togar_list = [ 1111, 2333, 443, 455, 6665 ] 
+togar_list1 = togar_list    
+print ( togar_list is togar_list1 ) # True
+
+
+
