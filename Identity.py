6ab04f618aceff7e41119daf3048f16f5b5f135a
Wilmot2025 <wilmotokai@gmail.com>
2025-11-18 14:38:01 +0000

    using identity

---
+++ Identity.py
@@ -0,0 +1,5 @@
+# Identity operator : # => it is use for address of elements not the elements.
+# it compares the address of elements not the values or elements.
+suhani_list = [ 1, 2, 3, 4, 5 ]
+suhani_list1 = [ 1, 2, 3, 4, 5 ]    
+print ( suhani_list is suhani_list1 ) # False
