1b22dd69ce9ac20eb0068788e7b1a1c24f76ddac
Wilmot2025 <wilmotokai@gmail.com>
2025-11-19 14:05:07 +0000

    pratice

--- suqare.py
+++ suqare.py
@@ -3,3 +3,12 @@ def find_square():
     my_variable = int(input("HIi, Wilmot please enter any number to find sqr: "))
     print("hii WImot here is your sqr: ",my_variable * my_variable)
 find_square()
+
+
+def fine_square():
+    my_variable = int(input("HIi, Wilmot please enter any number to find sqr: "))
+    print("Hi WImot here is your sqr: ",my_variable ** 2)   
+
+    def find_square():
+        my_variable = int(input("Hey, Rich please enter any number to find sqr: "))
+        print("Hey Rich here is your sqr: ",my_variable * my_variable)  
