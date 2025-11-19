0b5083f68581728f66884526196cbaad8a93759a
Wilmot2025 <wilmotokai@gmail.com>
2025-11-19 14:11:51 +0000

    Praticing

--- even and odd Nmber.py
+++ even and odd Nmber.py
@@ -16,5 +16,39 @@ for ch in string:
     if ch in 'aeiou':
         print(ch)
 
+def check_even_odd(x):
+    if x % 2 == 0:
+             print("this is even number ")
+    else:
+             print("this is odd number ")   
 
-        
+# 889 % 2 => 1
+# 99 % 2 => 1  => odd number
+
+#88 % 2 = 0
+#12 % 2 = 0  => even number 
+#14 % 2 = 0  => even number 
+
+def check_even_odd(x):
+   
+    if x % 2 == 0:
+             print("this is even number ")
+    else:
+             print("this is odd number ")
+  
+
+
+def check_even_odd(x):
+    if x % 2 == 0:
+             print("this is even number ")
+    else:
+             print("this is odd number ")
+
+def check_odd_even(x):
+    if x % 1 == 0:
+             print("this is odd number ")
+    else:
+             print("this is even number ")
+
+
+     
