ed1f2bce72cb8929964eb632b3322e617d3f6191
Wilmot2025 <wilmotokai@gmail.com>
2025-11-21 13:17:28 +0000

    excuting input with  if elif function

--- if else.py
+++ if else.py
@@ -68,5 +68,12 @@ if age >= 18 :
 else: 
         print("you are not eligigble to vote")
 
-
-        
+score = int(input("Enter your score: "))    
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")     
