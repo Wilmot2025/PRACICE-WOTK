f7a266eef12b2a4339e92e9dc3930686f861411f
Wilmot2025 <wilmotokai@gmail.com>
2025-11-22 14:02:33 +0000

    using even to check in function

--- Functions.py
+++ Functions.py
@@ -83,3 +83,13 @@ def even_check(i):
         print("odd")
 v = int(input("Enter Number -> "))
 even_check(v)
+
+#second one
+def even_check(a,b):
+    for i in range(a,b):
+        if i%2==0:
+           print(i)
+x=10
+y=100
+print(f"Even no. from {x} and {y}:- ")
+even_check(x,y)
