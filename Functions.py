e201cf4258a6090dccd873d29ed6730f5a6054a8
Wilmot2025 <wilmotokai@gmail.com>
2025-11-22 14:03:55 +0000

    using ranging in input in function

--- Functions.py
+++ Functions.py
@@ -93,3 +93,13 @@ x=10
 y=100
 print(f"Even no. from {x} and {y}:- ")
 even_check(x,y)
+
+def even_check(a,b):
+    for i in range(a,b):
+        if i%2==0:
+           print(i,end='')
+           print(" ",end='')
+x = int(input("Enter starting range -> "))
+y = int(input("Enter ending range-> "))
+print(f"Even no. from {x} and {y}:- ")
+even_check(x,y)
