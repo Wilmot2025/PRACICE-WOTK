a7c7aa24a1f408e930e82f3d4c3796d8aae0973b
Wilmot2025 <wilmotokai@gmail.com>
2025-11-24 14:02:16 +0000

    function usages

--- Task.py
+++ Task.py
@@ -9,3 +9,10 @@ print_your_age()
 def print_wife_name():
     print("her name is Saybioh")
 print_wife_name()  
+
+def print_wife_age():
+    print("she is 18 years old")    
+print_wife_age()    
+def print_children_name(): 
+    print("my children name are chersih and Ritchie")
+print_children_name()
