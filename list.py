ce223fbe382833dc04b166adad04b2316f1a2a7f
Wilmot2025 <wilmotokai@gmail.com>
2025-11-25 15:13:08 +0000

    working with list

---
+++ list.py
@@ -0,0 +1,24 @@
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+
+my_list.append(11)
+print(my_list)
+
+
+my_list.extend([12,13,14,15,16,17,18,19,20])
+print(my_list)  
+
+my_list.insert(0,777) 
+print(my_list)
+
+my_list.remove(777)
+print(my_list)
+
+my_list.pop()
+print(my_list) 
+
+my_list.index(18)  
+print(my_list)  
+
+v = my_list.count(12)
+print(v)
