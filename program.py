4c8714a30fa02692e029de4906ba60e08b1315c2
Wilmot2025 <wilmotokai@gmail.com>
2025-11-26 13:54:54 +0000

    program to add two numbers

--- program.py
+++ program.py
@@ -2,5 +2,13 @@
 def name():
     print("My name is Wilmot T. Okai") 
 
+# program to write enter interger and enter by the user.
 number = int(input("Enter an integer: "))
 print("You entered:", number)
+
+# program to add two numbers.
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+sum = a + b
+print("Sum =", sum)
