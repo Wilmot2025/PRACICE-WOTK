dd5de5b96ac401c042e4d72de84d1207f9fdb3b2
Wilmot2025 <wilmotokai@gmail.com>
2025-11-26 14:04:34 +0000

    checking for prime number

--- program.py
+++ program.py
@@ -9,6 +9,11 @@ print("You entered:", number)
 # program to add two numbers.
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-
 sum = a + b
 print("Sum =", sum)
+
+# program to check if a number is prime or not.
+n = int(input())
+print("Prime" if n>1 and all(n%i for i in range(2, int(n**0.5)+1)) else "Not Prime")
+
+
