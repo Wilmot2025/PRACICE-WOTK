afc0d4f086cb217c34147a497e6eb124c6321b3e
Wilmot2025 <wilmotokai@gmail.com>
2025-11-26 18:23:59 +0000

    using import

--- split.py
+++
@@ -1,7 +0,0 @@
-sentence = "Welcome to python programming"
-print("output of split is : ", sentence.split())  # ['Welcome', 'to', 'python', 'programming']
-
-std_name = ['Wilmot', 'Sharik', 'Pandey']
-print("output of join is : ", "--".join(std_name))  # Wilmot--Sharik--Pandey
-# Seperator => "--"
-my_string = "HelloHi"
