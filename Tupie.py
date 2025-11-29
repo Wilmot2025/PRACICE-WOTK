a642db35f5193e102b229302f4316681a9d5a55e
Wilmot2025 <wilmotokai@gmail.com>
2025-11-29 15:07:16 +0000

    Applying Tuplies defination

--- Tupie.py
+++ Tupie.py
@@ -1,18 +1,6 @@
 # woring with tupie
-numone = ('abcdefghij')
-print(numone[1:5])  
-
-lenth = len(numone)     
-print(lenth)
-
-numtwo = (1,2,3,4,5,6,7,8,9,10)
-print(numtwo[2:5])  
-
-numthree = (1,2,3,4,5,6,7,8,9,10)   
-print(numthree [1:5:2]) 
-
-
-num=(3,)    
+# tuple is a collection of data which is ordered and unchangeable
+num=(3)  
 print(num)
 
 num=(3,4,5,6,7,8,9,10)    
