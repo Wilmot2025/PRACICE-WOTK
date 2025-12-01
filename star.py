076cd2d2e56624233c8a277a091a46512f49c106
Wilmot2025 <wilmotokai@gmail.com>
2025-12-01 13:38:54 +0000

    using star

---
+++ star.py
@@ -0,0 +1,13 @@
+# how to used star in python
+
+print("**********")
+print("*        *") 
+
+n=5
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
