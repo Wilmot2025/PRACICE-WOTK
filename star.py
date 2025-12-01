5ebaf0567b5e08edf8fbcb9ced5702e71f9043c3
Wilmot2025 <wilmotokai@gmail.com>
2025-12-01 13:44:04 +0000

    Using condition statement with Start

--- star.py
+++ star.py
@@ -4,10 +4,10 @@ print("**********")
 print("*        *") 
 
 n=5
-
 for i in range(n):
-    for j in range(n-i-1):
+    for i in range(n):
         print(" ",end="")
-    for j in range(2*i+1):
+    for i in range(n-i):  
         print("*",end="")
-    print()
+    print() 
+    
