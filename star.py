497b0f642708578bc2a76c576b5ccc7ace51f6d7
Wilmot2025 <wilmotokai@gmail.com>
2025-12-01 13:51:37 +0000

    Praticing

--- star.py
+++ star.py
@@ -9,5 +9,14 @@ for i in range(n):
         print(" ",end="")
     for i in range(n-i):  
         print("*",end="")
-    print() 
+    print()     
+    
+n = 5
+for i in range(n):
+    for i in range(n+i):
+        print(" ",end="")
+    for j in range(n+1):
+        print("*",end="")
+    print()
+
     
