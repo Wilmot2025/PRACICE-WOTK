e75c52f7bdc4d01108c758469dca898bc56f2339
Wilmot2025 <wilmotokai@gmail.com>
2025-12-03 17:08:23 +0000

    global variable

---
+++ oops.py
@@ -0,0 +1,13 @@
+rainer = "Shashant"  ## global varible
+
+def total_students():
+                    std_class = "BCA"  # local variable
+
+                    print("WIlmot")
+                    print("SHarik")
+                    print("Aakansha")           
+                    print(rainer)
+                    print(std_class)
+total_students()    
+
+print(rainer)
