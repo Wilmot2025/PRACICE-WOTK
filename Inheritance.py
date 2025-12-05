e02a38838dcd9bd7bc5f9bfcfb001293b26dde9b
Wilmot2025 <wilmotokai@gmail.com>
2025-12-05 14:38:02 +0000

    praticing  inheritance

---
+++ Inheritance.py
@@ -0,0 +1,23 @@
+class Father:
+                   father_1st_property = "Taj Hotel"
+
+def father_assets(self):
+                           print("Bhardwaj aprament")
+                           print("Willing Restaurant")
+
+
+father_obj =  Father() 
+
+# print(father_obj.father_1st_property)
+# father_obj.father_assets()
+
+class Son(Father):
+    def son_assets(self):
+       
+# Create an instance of the Son class
+            son_instance = Son()
+ # Call the son_assets method
+            son_instance.son_assets()
+# Type of class  parent class,( base or super class
+# child class (derived or sub class
+#The class whos properities and mehtods are inherited is called pare
