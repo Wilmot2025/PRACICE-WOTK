81770a3380fd3ff0647044a6d431a21ace6bb033
Wilmot2025 <wilmotokai@gmail.com>
2025-12-07 16:09:37 +0000

    types of inheritance

--- Tupie.py
+++ Tupie.py
@@ -1,9 +1,10 @@
-# woring with tupie
-# tuple is a collection of data which is ordered and unchangeable
-num=(3)  
-print(num)
+# single_Element_tuple = (5,)
+# print(type(single_Element_tuple))
 
-num=(3,4,5,6,7,8,9,10)    
-print(num [7]) 
-length = len(num)
-print(length)
+# empty_tuple = ()
+
+my_tuple = (55, 77, 35, 99, 222, 66)#tuple
+print(my_tuple[2]) 
+
+my_tuple = (55, 44, 55, 22, 22)
+print(my_tuple.count(55))   
