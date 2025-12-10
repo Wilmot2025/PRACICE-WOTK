5ad01735dd3c9293086bb42a364e4cfa14e212d0
Wilmot2025 <wilmotokai@gmail.com>
2025-12-10 14:52:31 +0000

    praticing

--- Single inheritance.py
+++ Single inheritance.py
@@ -1,7 +1,29 @@
 class grandFather:
-     grandFather = "Blue Lake Place"
+     grandFather_property = "Blue Lake Place"
      def grandfather_assets(self):
-         print("his property")
+         print("his property") 
+
+class Father(grandFather):
+    Father_property = "Black and white hotel"
+    def father_assets(self):
+        print("father hotel")
+
+class Son(Father,grandFather):
+    Son_property = "Royal Palace"
+    def son_assets(self):
+        print("his property")
+
+obj_Son = Son() 
+print(obj_Son.grandFather_property)
+obj_Son.grandfather_assets()
+print(obj_Son.Father_property)
+obj_Son.father_assets()
+print(obj_Son.son_property)
+obj_Son.son_assets()
+                  
+  
+
+
 
          
 
